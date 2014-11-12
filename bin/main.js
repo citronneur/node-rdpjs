88e18b771d60e3d32fd352875115e92183a5884c
speyrefitte <citronneur@gmail.com>
2014-11-12 18:20:58 +0100

    begin engine

--- bin/main.js
+++ bin/main.js
@@ -1,4 +1,16 @@
 /**
- * New node file
+ * test file
  */
 var rdp = require("../lib");
+
+function Factory() {
+	
+}
+
+Factory.prototype = {
+	buildProtocol : function(socket) {
+		return new rdp.layer.BufferLayer(socket, rdp.rdp.TPKT(new rdp.rdp.X224.Client()));
+	}
+};
+
+new rdp.net.Reactor(new Factory()).connect('wav-glw-009', 3389);
