dde8cd61388bfd5338dad8d346dddf4fafda403e
speyrefitte <citronneur@gmail.com>
2015-06-01 18:18:59 +0200

    debug mcs layer

--- bin/client.js
+++ bin/client.js
@@ -9,7 +9,7 @@ function Factory() {
 
 Factory.prototype = {
 	buildProtocol : function(socket) {
-		return new rdp.core.layer.BufferLayer(socket, new rdp.protocol.TPKT(new rdp.protocol.X224.Client()));
+		return new rdp.core.layer.BufferLayer(socket, new rdp.protocol.TPKT(new rdp.protocol.x224.Client(new rdp.protocol.t125.mcs.Client())));
 	}
 };
 new rdp.core.Reactor().connect('54.187.36.238', 3389, new Factory());
