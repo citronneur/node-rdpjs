ceb1b7b06de400ad190474891455bfc4700ea828
speyrefitte <citronneur@gmail.com>
2015-06-02 16:40:25 +0200

    do not use string in javascript for binary stream

--- bin/client.js
+++ bin/client.js
@@ -12,4 +12,4 @@ Factory.prototype = {
 		return new rdp.core.layer.BufferLayer(socket, new rdp.protocol.TPKT(new rdp.protocol.x224.Client(new rdp.protocol.t125.mcs.Client())));
 	}
 };
-new rdp.core.Reactor().connect('54.187.36.238', 3389, new Factory());
+new rdp.core.Reactor().connect('wav-glw-009', 3389, new Factory());
