d21b06e72ddfa51c5f860cb8b3bca28646dfa7f0
speyrefitte <citronneur@gmail.com>
2015-06-09 18:12:48 +0200

    finish mcs layer + remove old design to use event design (more javascript)

--- bin/client.js
+++ bin/client.js
@@ -3,13 +3,5 @@
  */
 var rdp = require("../lib");
 
-function Factory() {
-	
-}
 
-Factory.prototype = {
-	buildProtocol : function(socket) {
-		return new rdp.core.layer.BufferLayer(socket, new rdp.protocol.TPKT(new rdp.protocol.x224.Client(new rdp.protocol.t125.mcs.Client())));
-	}
-};
-new rdp.core.Reactor().connect('wav-glw-009', 3389, new Factory());
+rdp.createClient().connect('wav-glw-009', 3389);
