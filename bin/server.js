c28a08c0befc2358059b943a3270f4a169d82519
citronneur <citronneur@gmail.com>
2015-07-03 18:06:33 +0200

    add mcs server side

--- bin/server.js
+++ bin/server.js
@@ -5,8 +5,10 @@ var rdp = require("../lib");
 
 
 rdp.createServer({
-	key : '/home/sylvain/dev/node-rdp-cert/ryans-key.pem',
-	cert : '/home/sylvain/dev/node-rdp-cert/ryans-cert.pem'
+	//key : '/home/sylvain/dev/node-rdp-cert/ryans-key.pem',
+	//cert : '/home/sylvain/dev/node-rdp-cert/ryans-cert.pem'
+	key : '/home/speyrefitte/dev/node-rdp-cert/ryans-key.pem',
+	cert : '/home/speyrefitte/dev/node-rdp-cert/ryans-cert.pem'
 }, function (server) {
 	
 }).listen(33390);
